b56294d73179521553e756f56308e0db8d965083
Sindhu Krovvidi <sindhukrovvidi@gmail.com>
2024-04-17 01:46:20 -0400

    Added template for viewing all questions

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,22 @@ import ProtectedRoute from "./components/ProtectedRoute/index.js";
 import Login from "./components/Login/index.js";
 import SignUp from "./components/SignUp/index.js";
 import NewAnswer from "./components/NewAnswer/index.js";
+// import Question from "./components/QuestionPage/question/index.js";
+import QuestionPage from "./components/QuestionPage/index.js";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        {/* <FakeStackOverflow /> */}
+      <FakeStackOverflow>
         <Routes>
-          <Route path="/" element={<FakeStackOverflow />} />
           <Route exact path="/answer" element={<ProtectedRoute />}>
             <Route exact path="/answer" element={<NewAnswer />} />
           </Route>
           <Route exact path="/login" element={<Login navigateTo={"/"} />} />
           <Route exact path="/register" element={<SignUp />} />
+          <Route exact path="/questions" element={<QuestionPage />} />
         </Routes>
-      </div>
+      </FakeStackOverflow>
     </Router>
   );
 }
--- client/src/components/fakestackoverflow.js
+++ client/src/components/fakestackoverflow.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
+import SideBarNav from "./SideBarNav";
+// import { Route, Navigate , Routes} from "react-router-dom";
+import { useNavigate,  } from "react-router-dom";
 
-export default function fakeStackOverflow() {
+export default function fakeStackOverflow({children}) {
+    const [selectedTab, setSelectedTab] = useState('q');
+    const navigate = useNavigate();
+
+    const handleQuestions = () => {
+        setSelectedTab('q')
+        navigate("/questions")
+    }
+
+    const handleTags = () => {
+        setSelectedTab('t')
+    }
+
+    const handleFavourites = () => {
+        setSelectedTab('f')
+    }
+
     return (
-       <>
+       <div id="main-content" style={{height: '100vh'}}>
         <Header></Header>
-       </>
+        <div id="main" className="main">
+            <SideBarNav
+                selected={selectedTab}
+                handleQuestions={handleQuestions}
+                handleTags={handleTags}
+                handleFavourites={handleFavourites}
+            />
+            <div id="right_main" className="right_main">
+                {children}
+            </div>
+        </div>
+       </div>
     );
 }
